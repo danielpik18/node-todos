d0a8c2727da2cfde30027323d64d36c77295fbff
Daniel <danielmomusic@gmail.com>
2019-02-16 17:12:13 -0500

    make it better

--- server/server.js
+++ server/server.js
@@ -32,89 +32,102 @@ const port = process.env.PORT || 3000;
 //  --** TODOS **--
 
 //ADD NEW TODO
-app.post('/todos', authenticate, (req, res) => {
-
-    let newTodo = new Todo({
-        _creator: req.user._id,
-        description: req.body.description
-    });
-
-    newTodo.save().then(doc => {
-        res.send(doc);
-    }, error => {
-        res.status(400).send(error);
-    });
-
+app.post('/todos', authenticate, async (req, res) => {
+    try {
+        const newTodo = new Todo({
+            _creator: req.user._id,
+            description: req.body.description
+        });
+        const todo = await newTodo.save();
+        res.send(todo);
+    } catch (error) {
+        res.status(400).send({
+            error
+        });
+    };
 });
 
-app.get('/todos', authenticate, (req, res) => {
-    Todo.find({
-        _creator: req.user._id
-    }).then(todos => {
+app.get('/todos', authenticate, async (req, res) => {
+    try {
+        const todos = await Todo.find({
+            _creator: req.user._id
+        });
         res.send({
             todos
         });
-    }, error => {
-        res.status(400).send(error);
-    });
+    } catch (error) {
+        res.status(400).send({
+            error
+        });
+    };
+
 });
 
 //GET/todos/:id
-app.get('/todos/:id', authenticate, (req, res) => {
-    Todo.findOne({
-        _id: req.params.id,
-        _creator: req.user._id
-    }).then(todo => {
+app.get('/todos/:id', authenticate, async (req, res) => {
+    try {
+        const todo = await Todo.findOne({
+            _id: req.params.id,
+            _creator: req.user._id
+        });
         if (!todo) return res.status(404).send();
         res.send({
             todo
         });
-    }).catch(error => res.status(400).send({
-        error: error.message
-    }));
+    } catch (error) {
+        res.status(400).send({
+            error: error.message
+        });
+    }
 });
 
 // /DELETE/todos/:id
-app.delete('/todos/:id', authenticate, (req, res) => {
-    Todo.findOneAndDelete({
-        _id: req.params.id,
-        _creator: req.user._id
-    }).then(todo => {
+app.delete('/todos/:id', authenticate, async (req, res) => {
+    try {
+        const todo = await Todo.findOneAndDelete({
+            _id: req.params.id,
+            _creator: req.user._id
+        });
         if (!todo) return res.status(404).send();
         res.send({
             todo
         });
-    }).catch(error => res.status(400).send({
-        error: error.message
-    }));
+    } catch (error) {
+        res.status(400).send({
+            error: error.message
+        });
+    }
 })
 
-app.patch('/todos/:id', authenticate, (req, res) => {
-    let id = req.params.id;
-    let body = _.pick(req.body, ['done', 'description']);
+app.patch('/todos/:id', authenticate, async (req, res) => {
+    try {
+        let id = req.params.id;
+        let body = _.pick(req.body, ['done', 'description']);
 
-    if (_.isBoolean(body.done) && body.done) {
-        body.completedAt = new Date().getTime();
-    } else {
-        body.done = false;
-        body.completedAt = null;
-    }
+        if (_.isBoolean(body.done) && body.done) {
+            body.completedAt = new Date().getTime();
+        } else {
+            body.done = false;
+            body.completedAt = null;
+        }
 
-    Todo.findOneAndUpdate({
-        _id: id,
-        _creator: req.user._id
-    }, {
-        $set: body
-    }, {
-        new: true
-    }).then(todo => {
+        let todo = await Todo.findOneAndUpdate({
+            _id: id,
+            _creator: req.user._id
+        }, {
+            $set: body
+        }, {
+            new: true
+        });
         if (!todo) return res.status(404).send();
         res.send({
             todo
         });
-    }).catch(error => res.status(400).send({
-        error
-    }));
+    } catch (error) {
+        res.status(400).send({
+            error
+        })
+    }
 });
 
 
@@ -136,41 +149,44 @@ module.exports = {
 };
 
 //POST /users
-app.post('/users', (req, res) => {
-    let body = _.pick(req.body, ['email', 'password']);
-    let newUser = new User(body);
-
-    newUser.save().then(() => {
-        return newUser.generateAuthToken();
-    }).then(token => {
+app.post('/users', async (req, res) => {
+    try {
+        let body = _.pick(req.body, ['email', 'password']);
+        let newUser = new User(body);
+        await newUser.save();
+        const token = await newUser.generateAuthToken();
         res.header('x-auth', token).send(newUser);
-    }).catch(error => res.status(400).send({
-
-    }));
+    } catch (error) {
+        res.status(400).send({
+            error
+        });
+    }
 });
 
 //POST/users/login
-app.post('/users/login', (req, res) => {
-    let body = _.pick(req.body, ['email', 'password']);
-
-    User.findByCredentials(body.email, body.password).then(user => {
-        return user.generateAuthToken().then(token => {
-            res.header('x-auth', token).send({
-                user
-            });
+app.post('/users/login', async (req, res) => {
+    try {
+        const body = _.pick(req.body, ['email', 'password']);
+        const user = await User.findByCredentials(body.email, body.password);
+        const token = await user.generateAuthToken();
+        res.header('x-auth', token).send({
+            user
         });
-    }).catch(error => {
-        res.status(400).send();
-    });
+    } catch (error) {
+        res.status(400).send({
+            error
+        });
+    }
 });
 
 //DELETE/users/logout
-app.delete('/users/logout', authenticate, (req, res) => {
-
-    req.user.logout(req.token).then(() => {
+app.delete('/users/logout', authenticate, async (req, res) => {
+    try {
+        await req.user.logout(req.token)
         res.status(200).send();
-    }).catch(error => res.status(400).send({
-        error
-    }));
-
+    } catch (error) {
+        res.status(400).send({
+            error
+        });
+    };
 });
